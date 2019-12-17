8ac4d28ba21f56d3ee5f9c8e7e7836f94a6845a7
Nitin Chaurasia <gs.nitin@gmail.com>
2019-12-17 02:19:41 -0500

    Update CorsConfiguration.java

--- src/main/java/com/todo/app/CorsConfiguration.java
+++ src/main/java/com/todo/app/CorsConfiguration.java
@@ -10,7 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 
 @Configuration
-@EnableWebMvc
 public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
